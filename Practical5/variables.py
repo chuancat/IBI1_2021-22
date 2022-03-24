9c8b37047f2c5ac7893695e4e1038afccf7bdc73
cora <cora@Vanilla.local>
2022-03-24 21:34:14 +0800

    variable

---
+++ Practical5/variables.py
@@ -0,0 +1,23 @@
+#  2022 a <-- 19245301
+#  2021 b <-- 4218520
+#  2020 c <-- 271
+#  difference between the numbers of cases in 2020 and 2021 d
+#  difference between the numbers of cases in 2021 and 2022 e
+#  compare d to e
+#  calculate the rate m in 2020
+#  calculate the rate n in 2021
+#  compare
+
+a = 19245301
+b = 4218520
+c = 271
+d = b-c
+e = a-b
+
+# e was greater than d
+# m was greater than n
+
+x = True
+y = False
+w = x and y
+print(w)
