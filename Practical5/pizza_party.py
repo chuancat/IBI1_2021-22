53db80c78fceaca027078c208a4d741e99d64457
cora <cora@Vanilla.local>
2022-03-24 20:39:35 +0800

    pizza

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,15 +1,14 @@
 # Function: calculate the total number of pizza slices that can be cut 
 # from an increasing number of straight cut
-# calculate p with (n**2+n+2)/2
-# display p
-# check if p>=64
-# if yes: done
-# if no: let n+1 equals to n
+# check if (n**2+n+2)/2<64
+# if yes: let n+1 equals to n
+# if no: let (n**2+n+2)/2 equals to p
+#        display ("The number of pieces of pizza is"+ str(p))
 
 n=1
 while (n**2+n+2)/2<64:
       n+=1
       p = (n**2+n+2)/2
       
-print("The number of pieces of pizza is"+str(p))
+print("The number of pieces of pizza is"+ str(p))
       
