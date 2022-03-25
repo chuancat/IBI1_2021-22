c5c6718a7581f9bb4a7dc552046cfbd715d3f8a2
cora <cora@Vanilla.local>
2022-03-25 10:57:05 +0800

    average

--- Practical6/mark.py
+++ Practical6/mark.py
@@ -34,7 +34,7 @@ plt.show()
 mark = [45, 36, 86, 57, 53, 92, 65, 45]
 avg = sum(mark)/len(mark)
 if avg>=60:
-   print("pass"）
+   print( "pass" ）
 else:
-   print("fail")
+   print( "fail" )
      
