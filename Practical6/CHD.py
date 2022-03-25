47492d5966a21c41d95cddef6c52dfa83a0e2a45
cora <cora@Vanilla.local>
2022-03-25 09:05:14 +0800

    figure

--- Practical6/CHD.py
+++ Practical6/CHD.py
@@ -9,9 +9,9 @@ N = 10
 chd = (1.03, 1.07, 1.11, 1.17, 1.23, 1.32, 1.42, 1.55, 1.72, 1.94)
 paternal_age = (30, 35, 40, 45, 50, 55, 60, 65, 70, 75)
 
-ply.ylabel('chd')
-ply.xlabel('paternal_age')
-ply.title('chd of different ages')
+plt.ylabel('chd')
+plt.xlabel('paternal_age')
+plt.title('chd of different ages')
 plt.scatter(x, y, marker='o')
 
 plt.show()
