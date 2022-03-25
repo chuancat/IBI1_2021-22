a73f0a35e5fe8403f765f7ba242297aba5de61e0
cora <cora@Vanilla.local>
2022-03-25 10:35:25 +0800

    title

--- Practical6/mark.py
+++ Practical6/mark.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 n = 8
 mark = (45, 36, 86, 57, 53, 92, 65, 45)
+plt.title('Rob's marks')
 plt.boxplot(mark,
             vert = True,
             whis = 1.5,
