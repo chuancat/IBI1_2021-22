fab5ff71992e01fea863f433c033a2321157f23d
cora <cora@Vanilla.local>
2022-03-25 09:03:51 +0800

    figure

--- Practical6/CHD.py
+++ Practical6/CHD.py
@@ -8,9 +8,7 @@ import matplotlib.pyplot as plt
 N = 10 
 chd = (1.03, 1.07, 1.11, 1.17, 1.23, 1.32, 1.42, 1.55, 1.72, 1.94)
 paternal_age = (30, 35, 40, 45, 50, 55, 60, 65, 70, 75)
-ind = np.arrange(N)
 
-p1 = plt.bar(ind, chd, width, yerr=paternal_age)
 ply.ylabel('chd')
 ply.xlabel('paternal_age')
 ply.title('chd of different ages')
