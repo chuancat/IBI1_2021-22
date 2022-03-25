0ce19549bcb5fd688f0965d10a54308efe7bc48f
cora <cora@Vanilla.local>
2022-03-25 10:45:51 +0800

    judge

--- Practical6/mark.py
+++ Practical6/mark.py
@@ -7,8 +7,10 @@ L = sorted(mark)
 print(L)
 
 #function:boxplot
+#import module
 import numpy as np
 import matplotlib.pyplot as plt
+#import parameter
 n = 8
 mark = (45, 36, 86, 57, 53, 92, 65, 45)
 plt.title('Rob's marks')
@@ -23,3 +25,15 @@ plt.boxplot(mark,
             notch = False  
                )
 plt.show()
+
+#pass
+#calculate the average mark
+#check  if it is higher than 60
+# if yes: print pass
+# if no: print fail
+mark = [45, 36, 86, 57, 53, 92, 65, 45]
+avg = sum(mark)/len(mark)
+if avg>=60
+   print(“pass”）
+else print("fail")
+     
