4352ab485fdf13aebc68fa3deea947dc049fd713
cora <cora@Vanilla.local>
2022-03-25 10:15:48 +0800

    list

--- Practical6/CHD.py
+++ Practical6/CHD.py
@@ -18,8 +18,14 @@ plt.scatter(x, y, marker='o')
 
 plt.show()
 
-# create a variable x of the paternal age
-paternal_age = {'30':1.03, '35':1.07, '40':1.11, '45':1.17, '50':1.23,
-'55':1.32, '60':1.42, '65':1.55, '70':1.72, '75':1.94}
-print(paternal_age['30'])
+# create a variable x of paternal age
+# create a list of age
+# create a list of risk
+# find the number of age 
+# display the risk it reflects
+x = 30
+L1 = [30, 35, 40, 45, 50, 55, 60, 65, 70, 75]
+L2 = [1.03, 1.07, 1.11, 1.17, 1.23, 1.32, 1.42, 1.55, 1.72, 1.94]
+y = L1.index(x) 
+print(L2[y])
 
