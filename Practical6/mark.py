c1e45107d53395c42572a46cacf414cc9efa6531
cora <cora@Vanilla.local>
2022-03-25 10:33:41 +0800

    boxplot

--- Practical6/mark.py
+++ Practical6/mark.py
@@ -1,3 +1,24 @@
+# create a list of mark
+# sort the list
+# display the sorted list
+ 
 mark = [45,36,86,57,53,92,65,45]
 L = sorted(mark)
 print(L)
+
+#function:boxplot
+import numpy as np
+import matplotlib.pyplot as plt
+n = 8
+mark = (45, 36, 86, 57, 53, 92, 65, 45)
+plt.boxplot(mark,
+            vert = True,
+            whis = 1.5,
+            patch_artist = True,
+            meanline = True,
+            showbox = True,
+            showcaps = True,
+            showfliers = True,
+            notch = False  
+               )
+plt.show()
