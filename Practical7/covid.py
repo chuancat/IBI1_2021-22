c3073b8f2fee142c2004cf9725f2bfb21847720d
cora <cora@Vanilla.local>
2022-03-30 16:52:05 +0800

    find Afghanistan

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -17,5 +17,15 @@ covid_data = pd.read_csv("full_data.csv")
 # print(covid_data.iloc[8:18,0:4:2]
 
 # function: show "total_cases" for all rows corresponding to Afghanistan
+# read just the column "location" and all rows
+# create a Boolean that is True when the "location" is "Afghanistan", but 
+# false otherwise
+# use the Boolean to find exactly the row needed in the dataframe
+n = 0
+my_rows = []
+a = covid_data.loc[n,"location"] 
+for w = a == "Afghanistan"
+    my_rows.append(w)
+    n+=1 
 my_columns = [False, False, False, False, True, False]
-print(covid_data.loc["Afghanistan",my_columns])
+print(covid_data.iloc[my_rows,my_columns])
