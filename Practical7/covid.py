0bbec6f9b58569dfbddbe738e1e24c8a108795e5
cora <cora@Vanilla.local>
2022-04-01 16:52:49 +0800

    china

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -10,22 +10,37 @@ os.chdir("/Users/cora/Downloads")
 # object
 covid_data = pd.read_csv("full_data.csv")
 
-# function: show the first and third columns from rows 10-20, include 10 
+# function1: show the first and third columns from rows 10-20, include 10 
 # and 20 
 # because the first row is name and the second row's number is 0, so row 
 # 10 number is 8
-# print(covid_data.iloc[8:18,0:4:2]
+
+print(covid_data.iloc[8:19,0:4:2])
 
 # function: show "total_cases" for all rows corresponding to Afghanistan
 # read just the column "location" and all rows
+# create a list represent rows Afghanistan belongs
 # create a Boolean that is True when the "location" is "Afghanistan", but 
 # false otherwise
 # use the Boolean to find exactly the row needed in the dataframe
+
 n = 0
 my_rows = []
-a = covid_data.loc[n,"location"] 
-for w = a == "Afghanistan"
+for n in range(0,7996):
+    a = covid_data.loc[n,"location"]
+    w = a == "Afghanistan"
     my_rows.append(w)
     n+=1 
 my_columns = [False, False, False, False, True, False]
 print(covid_data.iloc[my_rows,my_columns])
+
+n = 0
+my_rows =[]
+for n in range(0,7996):
+    a = covid_data.loc[n,"location"]
+    w = a == "China"
+    my_rows.append(w)
+    n+=1
+my_columns = [True, False, True, True, False, False]
+china = covid_data.iloc[my_rows,my_columns]
+print(china)
