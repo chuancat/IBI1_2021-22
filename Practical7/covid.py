d971c325a12bbf841d36231cf376e3d37a128757
cora <cora@Vanilla.local>
2022-04-03 14:31:03 +0800

    boxplt

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -14,7 +14,6 @@ covid_data = pd.read_csv("full_data.csv")
 # and 20 
 # because the first row is name and the second row's number is 0, so row 
 # 10 number is 8
-
 print(covid_data.iloc[8:19,0:4:2])
 
 # function: show "total_cases" for all rows corresponding to Afghanistan
@@ -23,7 +22,6 @@ print(covid_data.iloc[8:19,0:4:2])
 # create a Boolean that is True when the "location" is "Afghanistan", but 
 # false otherwise
 # use the Boolean to find exactly the row needed in the dataframe
-
 n = 0
 my_rows = []
 for n in range(0,7996):
@@ -34,6 +32,9 @@ for n in range(0,7996):
 my_columns = [False, False, False, False, True, False]
 print(covid_data.iloc[my_rows,my_columns])
 
+# function:compute the mean new cases and new deaths in China
+# collect new cases and new deaths in China from covid_data
+# use numpy to compute mean new cases and new deaths
 n = 0
 my_rows =[]
 for n in range(0,7996):
@@ -41,6 +42,56 @@ for n in range(0,7996):
     w = a == "China"
     my_rows.append(w)
     n+=1
-my_columns = [True, False, True, True, False, False]
-china = covid_data.iloc[my_rows,my_columns]
-print(china)
+my_columns1 = [True, False, True, True, False, False]
+china = covid_data.iloc[my_rows,my_columns1]
+my_columns2 = [False, True, True]
+a = china.iloc[:,my_columns2]
+# learn np.mean() from https://cloud.tencent.com/developer/ask/sof/1091397 
+# in 2nd,Apr.
+print(np.mean(a))
+
+# function:plot the new cases and new deaths in China as a box plot
+n = 0
+A = []
+B = []
+for n in range(0,92): 
+   a = china.iloc[n,1]   
+   A.append(a)
+   n+=1
+plt.boxplot(A,
+            vert = True,
+            whis = 1.5,
+            patch_artist = True,
+            meanline = True,
+            showbox = True,
+            showcaps = True,
+            showfliers = True,
+            notch = False
+               )
+plt.title('new cases in China')
+plt.xlabel('new cases')
+
+plt.show()
+
+# function:plot the new deaths in China as a box plot
+n = 0
+B = []
+for n in range(0,92):
+   b = china.iloc[n,2]
+   B.append(b)
+   n+=1
+plt.boxplot(B,
+            vert = True,
+            whis = 1.5,
+            patch_artist = True,
+            meanline = True,
+            showbox = True,
+            showcaps = True,
+            showfliers = True,
+            notch = False
+               )
+plt.title('new deaths in China')
+plt.xlabel('new deaths')
+
+plt.show()
+
