1c65cf3a1f175772710a6b84de7296d81673641b
cora <cora@Vanilla.local>
2022-05-20 03:19:57 +0800

    calculate cuts for 64 slices

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,14 +1,13 @@
 # Function: calculate the total number of pizza slices that can be cut 
 # from an increasing number of straight cut
 # check if (n**2+n+2)/2<64
-# if yes: let n+1 equals to n
+# if yes: let n equals n+1 
 # if no: let (n**2+n+2)/2 equals to p
-#        display ("The number of pieces of pizza is"+ str(p))
+#        display the number of cuts required for 64 slices
 
 n=1
-while (n**2+n+2)/2<64:
+while (n**2+n+2)/2<64:    
       n+=1
-      p = (n**2+n+2)/2
-      
-print("The number of pieces of pizza is "+str(p))
+      p=(n**2+n+2)/2
+print("The number of cuts required for 64 slices is "+str(n))
       
